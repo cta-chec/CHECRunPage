23af101e3984d3845ace2da782c682542d68b85b
Samuel Flis <samuel.d.flis@gmail.com>
2019-09-25 12:17:09 +0200

    Removing unecessary copy

--- crundb/tests/test_parser.py
+++ crundb/tests/test_parser.py
@@ -1,6 +1,5 @@
 from crundb.core.parse import eval_tag_expr
 import pytest
-import copy
 @pytest.fixture
 def test_sets():
     whole = list(range(10))
@@ -32,22 +31,20 @@ def test_eval_tag_exprs_andnot(test_sets):
 
 def test_eval_tag_exprs_parentheses(test_sets):
     sets = test_sets
-    res1 = eval_tag_expr('(whole ! even) | fivesix',copy.copy(sets))
-    res2 = eval_tag_expr('whole ! (even | fivesix)',copy.copy(sets))
+    res1 = eval_tag_expr('(whole ! even) | fivesix',sets)
+    res2 = eval_tag_expr('whole ! (even | fivesix)',sets)
     assert res1 != res2, "`(whole ! even) | fivesix` is not equal to `whole ! (even | fivesix)`"
     assert sorted(list(res1)) == [1,3,5,6,7,9], "Correct result for: `(whole ! even) | fivesix`"
     assert sorted(list(res2)) == [1,3,7,9], "Correct result for: `whole ! (even | fivesix)`"
 
 def test_eval_tag_exprs_nested_parentheses(test_sets):
     sets = test_sets
-    res = eval_tag_expr('fivesix | (whole & (even & fivesix))',copy.copy(sets))
+    res = eval_tag_expr('fivesix | (whole & (even & fivesix))',sets)
     assert sorted(list(res)) == [5,6],"Correct result for: `fivesix | (whole & (even & fivesix))`"
-    res = eval_tag_expr('(fivesix | whole) & (even & fivesix)',copy.copy(sets))
+    res = eval_tag_expr('(fivesix | whole) & (even & fivesix)',sets)
     assert sorted(list(res)) == [6], "Correct result for: `(fivesix | whole) & (even & fivesix)`"
 def test_eval_tag_exprs_multi_parentheses(test_sets):
     sets = test_sets
-    res = eval_tag_expr('whole  & (even | fivesix | fivesix | fivesix) ! (odd | fivesix)',sets)
-    # res = eval_tag_expr('whole & (even | (fivesix | fivesix) | fivesix) ',sets)
-    # res = eval_tag_expr('even|odd|whole',sets)
+    res = eval_tag_expr('whole  & (even | fivesix | fivesix ) ! (odd | fivesix)',sets)
     assert sorted(list(res)) == [0,2,4,8]
 
